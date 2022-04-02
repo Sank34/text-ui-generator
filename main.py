a2cd31cdd9d0bb4a557c72ac31acab0618bd347e
Sank <71709931+Sank34@users.noreply.github.com>
2022-04-02 22:24:34 +0300

    new main file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import texts
 print("Salut,bun venit la configurarea propriului tau design,te rugam sa alegi din tip-urile noastre variate:")
 print("1.-------")
 print("2.+++++++")
@@ -9,43 +10,16 @@ if sel != "1" and sel != "2" and sel != "3" and sel != "4":
     print("EROARE-argumente invalide")
     sel = input("Scrie numarul variantei dorite: ")
 
-#default funct
-def mh1(x, y, z):
-    print("-----------------")
-    print("IP:", x)
-    print("Store:", y)
-    print("Portal:", z)
-    print("-----------------")
-def mh2(a, b, c):
-    print("+++++++++++++++++")
-    print("IP:", a)
-    print("Store:", b)
-    print("Portal:", c)
-    print("+++++++++++++++++")
-def mh3(d, e, v):
-    print("✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄")
-    print("IP:", d, "|")
-    print("Store:", e, "|")
-    print("Portal:", v, "|")
-    print("✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄")
-
-def mh4(l, m, n):
-    print("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠")
-    print("✑ IP:", l, "|")
-    print("✑ Store:", m, "|")
-    print("✑ Portal:", n, "|")
-    print("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠")
-
 
 #def print statement---mh(input("Alegeti ip-ul"), input("alegeti store ul:"), input("alegeti portalul"))
 if sel == "1":
-    mh1(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
+    texts.mh1(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
 elif sel == "2":
-    mh2(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
+    texts.mh2(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
 elif sel == "3":
-    mh3(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
+    texts.mh3(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
 elif sel == "4":
-    mh4(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
+    texts.mh4(input("Alegeti ip-ul: "), input("alegeti store ul: "), input("alegeti portalul: "))
 
 print("Iti place acest design?(da/nu)")
 var = input()
@@ -56,9 +30,7 @@ if var != "da" and var != "nu":
     var = input()
 
 if var == "da":
-    print("Multumim,nu uita sa dai star la proiect")
+    print("Multumim,nu uita sa dai star la proiect!:D")
 elif var == "nu":
-    print("Ce pacat...")
-    print("Daca mai ai sugestii de ui-uri,nu uita sa contactezi dezvoltatorul pe discord.")
-    print("Discord: Sanke</>#4704")
+    print("Ce pacat...\nDaca mai ai sugestii de UI-uri,nu uita sa contactezi dezvoltatorul pe discord.\nDiscord: Sanke#4704")
 
