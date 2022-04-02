e48b0d152034e6cf50d48792fdfb8c5c460e412a
Sank <71709931+Sank34@users.noreply.github.com>
2022-04-02 22:25:06 +0300

    dependecies

---
+++ texts.py
@@ -0,0 +1,26 @@
+#default funct
+def mh1(x, y, z):
+    print("-----------------")
+    print("IP:", x)
+    print("Store:", y)
+    print("Portal:", z)
+    print("-----------------")
+def mh2(a, b, c):
+    print("+++++++++++++++++")
+    print("IP:", a)
+    print("Store:", b)
+    print("Portal:", c)
+    print("+++++++++++++++++")
+def mh3(d, e, v):
+    print("✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄")
+    print("IP:", d, "|")
+    print("Store:", e, "|")
+    print("Portal:", v, "|")
+    print("✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄✄")
+
+def mh4(o, m, n):
+    print("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠")
+    print("✑ IP:", o, "|")
+    print("✑ Store:", m, "|")
+    print("✑ Portal:", n, "|")
+    print("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠")
